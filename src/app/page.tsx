bed9e43d28f741c598dfcbf2a23c0556efbf2d10
Marcos <marcostulli@hotmail.com>
2024-06-07 09:56:50 -0300

    Add: Theme Context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import HomePage from "@/components/home/Home";
+import ThemeContext from "@/theme";
 
 
 export default function Home() {
     return (
-        <HomePage />
+        <ThemeContext>
+            <HomePage />
+        </ThemeContext>
     );
 
 }
---
+++ src/theme.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+export enum Theme {
+    dark = 'dark',
+    light = 'light',
+}
+
+interface ILanguageContext {
+    theme: Theme;
+    setTheme: (newTheme: Theme) => void;
+    toggleTheme: () => void;
+    isChecked: boolean;
+}
+
+const defaultContext = {
+    theme: Theme.dark,
+    setTheme: (newTheme: Theme) => { },
+    toggleTheme: () => { console.log("Changed Theme"); },
+    isChecked: false
+};
+
+export const LanguageContext = React.createContext<ILanguageContext>(defaultContext);
+
+export interface IThemeProviderProps {
+    children: React.ReactNode;
+}
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+
+});
+
+
+const ThemeContext: React.FC<IThemeProviderProps> = ({ children }) => {
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    );
+};
+
+export default ThemeContext;
