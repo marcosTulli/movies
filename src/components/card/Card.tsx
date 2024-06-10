76ed7744a23ba7208b2800347adc8aedbb75b1ad
Marcos <marcostulli@hotmail.com>
2024-06-10 02:49:50 -0300

    Fix: Slider spacing

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { usePoster } from '@/hooks/queries';
 import Image from 'next/image';
 import { Container } from '@mui/material';
+import style from './Card.module.scss';
 
 export interface ICardProps {
     posterId: string;
@@ -9,9 +10,9 @@ export interface ICardProps {
 const Card: React.FC<ICardProps> = ({ posterId }) => {
     const { data: poster } = usePoster({ width: '200', id: posterId, enable: posterId.length > 0 });
     return (
-        <div>
+        <Container className={style.cardContainer}>
             {poster && <Image alt={'poster'} src={poster} height={300} width={200} />}
-        </div>
+        </Container>
     );
 };
 
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import Card from '../card/Card';
 import { IMovie } from '@/models/models';
 import { Container } from '@mui/material';
+import style from './Slider.module.scss';
 
 export interface ISliderProps {
     movies: IMovie[];
 }
 const Slider: React.FC<ISliderProps> = ({ movies }) => {
     return (
-        <Container sx={{ display: 'flex' }}>
+        <Container sx={{ display: 'flex', margin: '0', gap: '2rem' }}>
             {
-                movies?.map((movie) => {
+                movies?.slice(0, 8).map((movie) => {
                     return (
                         <Card key={movie.id} posterId={movie.poster_path} />
                     );
                 })
             }
-        </Container>
+        </Container >
     );
 };
 
