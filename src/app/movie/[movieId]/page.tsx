85ec01ac64768f91d0c5b412b29b96bf4a2e64e2
Marcos <marcostulli@hotmail.com>
2024-06-13 17:28:36 -0300

    Add: Back button

--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { useMovie, usePoster } from '@/hooks/queries';
 import { LANGUAGE } from '@/models/models';
 import Image from 'next/image';
+import { ArrowBack } from '@mui/icons-material';
+import { Button } from '@mui/material';
+import { useRouter } from 'next/navigation';
 
 
 
@@ -11,12 +14,21 @@ export interface IMovieDetailsParams {
 }
 
 const MovieDetails: React.FC<IMovieDetailsParams> = ({ params }): JSX.Element => {
+    const router = useRouter();
     const { movieId } = params;
     const { data } = useMovie({ language: LANGUAGE.EN, movie_id: parseInt(movieId) });
     const enablePoster = data !== undefined && data.poster_path.length > 0;
     const { data: poster } = usePoster({ width: '200', id: data?.poster_path, enable: enablePoster });
+
+    const handleBackClick = () => {
+        router.back();
+    };
+
     return (
         <div>
+            <Button title={'back'} onClick={handleBackClick} style={{ padding: '1rem' }}>
+                <ArrowBack />
+            </Button>
             {data &&
                 <div>
                     <h1>{data.title}</h1>
